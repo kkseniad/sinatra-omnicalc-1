71093fdc0a4f59e1861a5203949fa23c3af41227
Kseniia Kormalova <kormalovak@gmail.com>
2025-04-05 19:53:11 +0000

    square_done

--- app.rb
+++ app.rb
@@ -6,8 +6,15 @@ get("/") do
 end
 
 get("/square/new") do
-  
   erb(:square)
+end
+
+get("/square/results") do
+  @number = params.fetch("number").to_i
+
+  @square = @number ** 2
+
+  erb(:sq_results)
 end
 
 get("/square_root/new") do
